56daf7948c45aab9548712b5ab99b531f1a36ad7
eva-dgan <36070988@mail.ru>
2025-10-01 22:09:28 +0700

    add wchar_t

--- lab0.cpp
+++ lab0.cpp
@@ -4,10 +4,11 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <locale>
 
 class WordCleaner {
 private:
-    static void list(const std::string& word, std::map<std::string, int>& Words) {
+    static void list(const std::wstring& word, std::map<std::wstring, int>& Words) {
         if (!Words[word]) {
             Words[word] = 1;
         } else {
@@ -16,16 +17,19 @@ private:
     }
 
 public:
-    static std::string clean(std::string word, std::map<std::string, int>& Words) {
-        if (word[word.length() - 1] == ',' || word[word.length() -1] == '.' || word[word.length() - 1] == '!' || word[word.length() - 1] == '?') {
+    static std::wstring clean(std::wstring word, std::map<std::wstring, int>& Words) {
+        if (word[word.length() - 1] == L',' || word[word.length() -1] == L'.' || word[word.length() - 1] == L'!' || word[word.length() - 1] == L'?') {
             word.pop_back();
         }
 
-        for (size_t i = 0; i < word.length(); i++) {
-            if (word[i] >= 'A' && word[i] <= 'Z') {
-                word[i] = word[i] + 32;
-            }
+        if (word[0] == L',' || word[0] == L'.' || word[0] == L'!' || word[0] == L'?' || word[0] == L'"') {
+            word.erase(0, 1);
         }
+
+        for (wchar_t& c : word) {
+            c = static_cast<wchar_t>(towlower(c));
+        }
+
         list(word, Words);
         return word;
     }
@@ -33,24 +37,26 @@ public:
 
 class SortOutput {
 private:
-    static bool compare(const std::pair<std::string, int>& word1, const std::pair<std::string, int>& word2) {
+    static bool compare(const std::pair<std::wstring, int>& word1, const std::pair<std::wstring, int>& word2) {
         return word1.second > word2.second;
     }
 public:
-    static void sort(std::map<std::string, int>& Words, const std::string& output, int count) {
-        std::vector<std::pair<std::string, int>> vec(Words.begin(), Words.end());
+    static void sort(std::map<std::wstring, int>& Words, const std::string& output, int count) {
+        std::vector<std::pair<std::wstring, int>> vec(Words.begin(), Words.end());
         std::sort(vec.begin(), vec.end(), compare);
 
-        std::ofstream file(output);
+        std::wofstream file(output);
+        file.imbue(std::locale(""));
 
         if (!file.is_open()) {
+            std::cout << "Not open output" << std::endl;
             return;
         }
 
-        file << "Слово; Частота; Частота(в%)\n";
+        file << L"Слово; Частота; Частота(в%)\n";
 
         for (const auto& pair : vec) {
-            file << pair.first << ";" << pair.second << ";" << (((double)pair.second / count) * 100) << "\n";
+            file << pair.first << L";" << pair.second << L";" << (((double)pair.second / count) * 100) << L"%\n";
         }
 
         file.close();
@@ -58,19 +64,23 @@ public:
 };
 
 int main() {
+    std::locale::global(std::locale(""));
+
     std::string File = "input.txt";
-    std::ifstream file(File);
+    std::wifstream file(File);
+    file.imbue(std::locale());
     if (!file.is_open()) {
+        std::cout << "Not open input" << std::endl;
         return 1;
     }
 
-    std::map<std::string, int> Words;
+    std::map<std::wstring, int> Words;
     int count = 0;
-    std::string word;
+    std::wstring word;
     while (file >> word) {
         count++;
         if (!word.empty()) {
-            std::string cleanWord = WordCleaner::clean(word, Words);
+            std::wstring cleanWord = WordCleaner::clean(word, Words);
         }
     }
 
