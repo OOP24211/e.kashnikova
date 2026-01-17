5b279b8930a48be724b5b49f389489f83b3adf0e
eva-dgan <36070988@mail.ru>
2026-01-17 20:32:56 +0700

    Create sortOutput.h

---
+++ sortOutput.h
@@ -0,0 +1,21 @@
+//
+// Created by Эвелина on 17.01.2026.
+//
+
+#ifndef LAB0_SORTOUTPUT_H
+#define LAB0_SORTOUTPUT_H
+
+#include <string>
+#include <map>
+
+class SortOutput {
+private:
+    static bool compare(const std::pair<std::wstring, int>& word1,
+                        const std::pair<std::wstring, int>& word2);
+
+public:
+    static void sort(std::map<std::wstring, int>& Words,
+                     const std::string& output, int count);
+};
+
+#endif //LAB0_SORTOUTPUT_H
