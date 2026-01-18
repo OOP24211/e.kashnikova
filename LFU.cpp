022cf00f22948cb7302bde9f8d30dd6c27a80425
eva-dgan <36070988@mail.ru>
2026-01-18 20:56:14 +0700

    Update LFU.cpp

--- LFU.cpp
+++ LFU.cpp
@@ -3,100 +3,70 @@
 //
 
 #include "LFU.h"
+#include <list>
+#include <unordered_map>
 
-#include <iostream>
-
-LFUCache::LFUCache(int capacity) {
-    this->capacity = capacity;
-    minFreq = 0;
-}
+LFUCache::LFUCache(int capacity) : capacity(capacity), minFreq(0) {}
 
 LFUCache::~LFUCache() {
-    for (auto& pair : cacheMap) {
+    for (auto& pair : cache) {
         delete pair.second;
     }
-    for (auto& pair : freqMap) {
-        delete pair.second.first;
-        delete pair.second.second;
-    }
-}
-
-void LFUCache::add(Node *node, int freq) {
-    if (freqMap.find(freq) == freqMap.end()) {
-        Node *head = new Node(-1, -1);
-        Node *tail = new Node(-1, -1);
-        head->next = tail;
-        tail->prev = head;
-        freqMap[freq] = {head, tail};
-    }
-
-    Node *head = freqMap[freq].first;
-    Node *temp = head->next;
-    node->next = temp;
-    node->prev = head;
-    head->next = node;
-    temp->prev = node;
-}
-
-void LFUCache::remove(Node *node) {
-    Node *delPrev = node->prev;
-    Node *delNext = node->next;
-    delPrev->next = delNext;
-    delNext->prev = delPrev;
-}
-
-void LFUCache::updateFreq(Node *node) {
-    int oldFreq = node->cnt;
-    node->cnt++;
-
-    remove(node);
-
-    if (freqMap[oldFreq].first->next == freqMap[oldFreq].second) {
-        delete freqMap[oldFreq].first;
-        delete freqMap[oldFreq].second;
-        freqMap.erase(oldFreq);
-        if (minFreq == oldFreq) {
-            minFreq++;
-        }
-    }
-    add(node, node->cnt);
 }
 
 int LFUCache::get(int key) {
-    if (cacheMap.find(key) == cacheMap.end()) {
+    if (capacity == 0 || cache.find(key) == cache.end()) {
         return -1;
     }
 
-    Node *node = cacheMap[key];
-    int res = node->value;
-    updateFreq(node);
-    return res;
+    Node* node = cache[key];
+
+    freqMap[node->cnt].erase(iterMap[key]);
+
+    if (freqMap[node->cnt].empty()) {
+        freqMap.erase(node->cnt);
+        if (minFreq == node->cnt) {
+            minFreq++;
+        }
+    }
+
+    node->cnt++;
+
+    freqMap[node->cnt].push_front(node);
+    iterMap[key] = freqMap[node->cnt].begin();
+
+    return node->value;
 }
 
 void LFUCache::put(int key, int value) {
     if (capacity == 0) return;
 
-    if (cacheMap.find(key) != cacheMap.end()) {
-        Node *node = cacheMap[key];
-        node->value = value;
-        updateFreq(node);
-    } else {
-        if (cacheMap.size() == capacity) {
-            Node *node = freqMap[minFreq].second->prev;
-            cacheMap.erase(node->key);
-            remove(node);
-
-            if (freqMap[minFreq].first->next == freqMap[minFreq].second) {
-                freqMap.erase(minFreq);
-            }
-            delete node;
-        }
-
-        Node *node = new Node(key, value);
-        cacheMap[key] = node;
-        minFreq = 1;
-        add(node, 1);
+    if (cache.find(key) != cache.end()) {
+        cache[key]->value = value;
+        get(key);  
+        return;
     }
+
+    if (cache.size() >= capacity) {
+        Node* toDelete = freqMap[minFreq].back();
+        freqMap[minFreq].pop_back();
+
+        cache.erase(toDelete->key);
+        iterMap.erase(toDelete->key);
+        delete toDelete;
+
+        if (freqMap[minFreq].empty()) {
+            freqMap.erase(minFreq);
+        }
+    }
+
+    Node* newNode = new Node(key, value);
+    newNode->cnt = 1;
+
+    cache[key] = newNode;
+    freqMap[1].push_front(newNode);
+    iterMap[key] = freqMap[1].begin();
+    minFreq = 1;
 }
 
 int LFUCache::operator[](int key) {
